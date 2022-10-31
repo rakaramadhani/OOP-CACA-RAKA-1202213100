d59e1f6c856a3cbb2cffc513ebd565e57be27873
rakaramadhani <100126637+rakaramadhani@users.noreply.github.com>
2022-10-31 08:44:39 +0700

    Add files via upload

---
+++ OOP_CACA_RAKA_1202213100_SSMODUL1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Praktikum1  
+{  
+public static void main(String[] args)
+{
+    System.out.println("Selamat Datang di Restaurant EAD");
+    System.out.println("Silahkan Register Terlebih Dahulu");
+    System.out.println("=================================");
+    public class variabel{
+        String nama;
+        String nohp;
+        public string getNama(){
+            System.out.println("Raina_SI4507");
+        public string getNohp(){
+            System.out.println("Raina_SI4507");
+
+    }
+    
+
+
+    }
+}
+
+}  
+}
